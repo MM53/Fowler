694031abf077759a57f420304437312199e87762
MM53 <28218664+mm53@users.noreply.github.com>
2020-05-17 10:53:33 +0200

    Move getCharge to Movie

--- src/Movie.java
+++ src/Movie.java
@@ -22,5 +22,23 @@ public class Movie {
         return title;
     }
 
-    ;
+    public double getCharge(int daysRented) {
+        double thisAmount = 0;
+        switch (priceCode) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (daysRented > 2)
+                    thisAmount += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (daysRented > 3)
+                    thisAmount += (daysRented - 3) * 1.5;
+                break;
+        }
+        return thisAmount;
+    }
 }
