c8f3c3a27b45431fc6a2e968116fac73cd67b8a9
MM53 <28218664+mm53@users.noreply.github.com>
2020-05-17 10:38:11 +0200

    Migrate rentals from Vector to List and replace while lop with for loop

--- src/Customer.java
+++ src/Customer.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 class Customer {
     private String name;
-    private Vector rentals = new Vector();
+    private List<Rental> rentals = new ArrayList<>();
 
     public Customer(String newname) {
         name = newname;
@@ -13,7 +13,7 @@ class Customer {
     ;
 
     public void addRental(Rental arg) {
-        rentals.addElement(arg);
+        rentals.add(arg);
     }
 
     ;
@@ -27,13 +27,11 @@ class Customer {
     public String statement() {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
-        Enumeration enum_rentals = rentals.elements();
         String result = "Rental Record for " + this.getName() + "\n";
         result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
-        while (enum_rentals.hasMoreElements()) {
+        for (Rental each : rentals) {
             double thisAmount = 0;
-            Rental each = (Rental) enum_rentals.nextElement();
             //determine amounts for each line
             thisAmount = amountFor(each);
             // add frequent renter points
